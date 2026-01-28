874ee667440322c895888f65d358c2a5d4510df5
Brayanklein555 <brayanprogramacao@gmail.com>
2026-01-28 19:15:51 -0300

    Update character.js

--- js/character.js
+++ js/character.js
@@ -1,1 +1,39 @@
+const Inventory={
+list:JSON.parse(localStorage.getItem("inv")||"[]"),
+render(){
+inventoryList.innerHTML="";
+this.list.forEach((i,idx)=>{
+let li=document.createElement("li");
+li.textContent=i;
+li.onclick=()=>{this.list.splice(idx,1);this.save();}
+inventoryList.appendChild(li);
+});
+},
+add(){
+if(itemInput.value){
+this.list.push(itemInput.value);
+itemInput.value="";
+this.save();
+}
+},
+save(){
+localStorage.setItem("inv",JSON.stringify(this.list));
+this.render();
+}
+};
 
+const Avatar={
+upload(e){
+let r=new FileReader();
+r.onload=()=>avatarPreview.src=r.result;
+r.readAsDataURL(e.target.files[0]);
+}
+};
+
+function rollAttribute(attr){
+const v=Number(document.getElementById(attr).value);
+const r=rollDice(20,v);
+alert("Maior resultado: "+r.max);
+}
+
+Inventory.render();
